27d88c6c13e056b360aa1dd4810d7f803c11ec88
Shourabh-Lodhi <lodhishoruabh786@gmail.com>
2022-12-08 04:39:12 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/demo", function(req, res) {
+  // console.log(req.body);
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.Email;
+
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = process.env.URL_UNIQUE_ID;
+
+  const options = {
+    method: "POST",
+    auth: process.env.API_KEY
+  }
+
+  const request = https.request(url, options, function(response) {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data) {
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(req, res) {
+  console.log("Server is running on port 3000");
+});
